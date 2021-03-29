c6de2874ff17bef2bd690c24acee79f89def38df
jmenend4 <jmenend4@gmail.com>
2021-03-29 09:32:28 -0300

    Initial project commit

---
+++ src/main/java/com/juan/spidergames/SpidergamesApplication.java
@@ -0,0 +1,13 @@
+package com.juan.spidergames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpidergamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpidergamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/spidergames/SpidergamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.spidergames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpidergamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
