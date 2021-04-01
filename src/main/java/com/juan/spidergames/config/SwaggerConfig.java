2e29e52a1077552a267104befed0bff7299e0200
jmenend4 <jmenend4@gmail.com>
2021-04-01 08:39:01 -0300

    Added some documentation

--- src/main/java/com/juan/spidergames/config/SwaggerConfig.java
+++ src/main/java/com/juan/spidergames/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.juan.spidergames.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,8 +20,18 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.juan.spidergames.resources"))
-				.paths(PathSelectors.any())
-				.build();
+				.paths(PathSelectors.ant("/api/**"))
+				.build().apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	      "Spider Games Persistence API", 
+	      "Backend support for Spider Games persistence requirements.", 
+	      "0.0.1", 
+	      "https://github.com/jmenend4/spider-games-java/blob/main/README.md",
+	      new Contact("Juan Menendez", "https://github.com/jmenend4", "jmenend4@gmail.com"), 
+	      "License of API", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
 	}
 
 }
--- src/main/java/com/juan/spidergames/utils/SpiderGamesMapper.java
+++ src/main/java/com/juan/spidergames/utils/SpiderGamesMapper.java
@@ -11,20 +11,70 @@ import com.juan.spidergames.api.paging.PageResponseDTO.ResultPage;
 import com.juan.spidergames.domain.kakuro.Kakuro;
 import com.juan.spidergames.domain.kakuro.cell.KakuroCell;
 
+/**
+ * The purpose of this class is to provide the conversion methods from API transfer objects to database documents
+ * and viceversa. It also provides the appropriate collection mappings.
+ * @author Juan Menendez
+ *
+ */
 public interface SpiderGamesMapper {
 
+	/**
+	 * Map the kakuro data transfer object to a kakuro document object in order to be persisted
+	 * @param kakuro
+	 * @return kakuro document
+	 */
 	Kakuro mapToEntity(KakuroDTO kakuro);
-
+	
+	/**
+	 * Map a kakuro grid of transfer object cells to a grid of db document cells
+	 * @param grid
+	 * @return a list of lists of document cells representing the kakuro grid to be persisted
+	 */
 	List<List<KakuroCell>> mapToEntity(List<List<KakuroCellDTO>> grid);
 
+	/**
+	 * Maps a single row of a kakuro grid of transfer objects to the corresponding row of document objects
+	 * @param row
+	 * @return a row of document kakuro cells to be persisted as part of a grid
+	 */
 	List<KakuroCell> mapKakuroRowToEntity(List<KakuroCellDTO> row);
 
+	/**
+	 * Map a single cell of a kakuro transfer object grid to a cell of document object to be persisted
+	 * @param cell
+	 * @return a document cell to be persisted as part of a grid
+	 */
 	KakuroCell mapToEntity(KakuroCellDTO cell);
 
+	/**
+	 * Maps a document kakuro that was gathered from the db to a transfer object in order to be used as a
+	 * response in an REST API
+	 * @param kakuro
+	 * @return a kakuro ready to be placed in an REST API response object
+	 */
 	KakuroDTO mapToApi(Kakuro kakuro);
 
+	/**
+	 * A generic list mapper. Maps from one list to another of the same size but containing objects result of the
+	 * provided mapper.
+	 * @param <T> Type of the object to be mapped
+	 * @param <V> Type of the resulting mapped object
+	 * @param ts List of T objects to be mapped to another type
+	 * @param mapper Function that converts from type T to V
+	 * @return a new list of the same size of ts with objects of type V
+	 */
 	<T, V> List<V> mapList(List<T> ts, Function<T, V> mapper);
 
+	/**
+	 * A generic page mapper. Maps from a Page object of type T to a Page with content of type V
+	 * of the same size resulting of mapping T to V using the provided mapper
+	 * @param <T> Type of the object to be mapped
+	 * @param <V> Type of the resulting mapped object
+	 * @param page Page of T objects to be mapped to another type
+	 * @param contentMapper Function that converts from type T to V
+	 * @return a new list of the same size of page containing objects of type V
+	 */
 	<T, V> ResultPage<V> mapToPageApi(Page<T> page, Function<T, V> contentMapper);
 
 }
